40fd537b4614434c4fc6be4581af4d16eb6ccd09
saiba09 <saiba_sheikh@persistent.co.in>
2017-11-13 16:42:55 +0530

    structure defined

---
+++ src/main/java/com/example/Data.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.json.simple.JSONObject;
+
+public class Data {
+
+	public static JSONObject getHolidays() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
